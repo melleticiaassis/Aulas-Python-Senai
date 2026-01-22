c533992d1ef376f62e0f61f397b8c8d370c63aa1
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-22 11:48:21 -0300

    Add factorial calculation with input validation

--- while_for.py/fatorial.py
+++ while_for.py/fatorial.py
@@ -1,2 +1,12 @@
 print("Olá!")
 n = int(input("Digite um número a ser calculado seu fatorial: "))
+
+if n < 0:
+    print("Número inválido")
+else:
+    fatorial = 1
+i = 1
+while i <= n:
+    fatorial = fatorial*i
+    i = i +1
+    print("Fatorial:", fatorial)
