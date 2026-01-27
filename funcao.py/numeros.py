7df05b8c0fd028a73c7c7d3c86ec14404dfd260a
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-27 11:52:17 -0300

    Add files via upload

---
+++ funcao.py/numeros.py
@@ -0,0 +1,7 @@
+# Mostrar números de 1 a 20 um abaixo do outro
+for i in range(1, 21):  # números de 1 a 20
+    print(i)/n            #  cada número vai para uma nova linha
+
+# Mostrar números de 1 a 20 um ao lado do outro
+for i in range(1, 21):
+    print(i, end=" ")    # 'end=" "' faz com que os números fiquem na mesma linha
---
+++ funcao.py/soma_media.py
@@ -0,0 +1,10 @@
+soma = 0 
+for i in ranger (5):
+        numero = int(input(f"Digite o {i+1}° numero "))
+        soma += numero
+
+# média dos valores
+media = media + soma / 5 
+
+print(f"A soma dos números é {soma}")
+print(f"A média dos números é {media}")
