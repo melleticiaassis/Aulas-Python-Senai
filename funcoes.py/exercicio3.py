40b2cf0e02fa70f00f1e0e4f3ddd4739e52eff65
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-28 12:14:13 -0300

    Refactor soma_numero to include input inside function

--- funcoes.py/exercicio3.py
+++ funcoes.py/exercicio3.py
@@ -1,9 +1,9 @@
-num1 = int(input("Digite um valor: "))
-num2 = int(input("Digite o segundo valor: "))
-
-soma = num1 + num2
 
 def soma_numero():
+    num1 = int(input("Digite um valor: "))
+    num2 = int(input("Digite o segundo valor: "))
+
+    soma = num1 + num2
     print(f"A soma dos valores é {soma}")
     
 soma_numero()
