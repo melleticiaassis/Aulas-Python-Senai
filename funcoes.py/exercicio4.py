2b535a215ecb9fa411a0814aaf514c95dbb19874
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-28 12:15:50 -0300

    Refactor calcular_media to include input inside function

--- funcoes.py/exercicio4.py
+++ funcoes.py/exercicio4.py
@@ -1,11 +1,12 @@
-nota1 = float(input("Digite sua primeira nota: "))
-nota2 = float(input("Digite sua segunda nota: "))
-nota3 = float(input("Digite sua terceira nota: "))
-nota4 = float(input("Digite sua quarta nota: "))
 
-media = (nota1 + nota2 + nota3 + nota4) /4
 
 def calcular_media():
+    nota1 = float(input("Digite sua primeira nota: "))
+    nota2 = float(input("Digite sua segunda nota: "))
+    nota3 = float(input("Digite sua terceira nota: "))
+    nota4 = float(input("Digite sua quarta nota: "))
+
+    media = (nota1 + nota2 + nota3 + nota4) /4
     print()
     print("Olá!")
     print(f"Sua média é {media}")
