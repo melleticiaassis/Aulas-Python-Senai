45eaef08105a95bf779968b01e28f65bc4ba17fb
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-28 12:17:44 -0300

    Refactor converter_metros to take input inside function

--- funcoes.py/exercicio5.py
+++ funcoes.py/exercicio5.py
@@ -1,8 +1,9 @@
-metros = float(input("Digite o valor em metros: "))
-
-centimetros = metros * 100
 
 def converter_metros ():
+    metros = float(input("Digite o valor em metros: "))
+
+    centimetros = metros * 100
+
     print(f"A conversão de metros para centimetros é {centimetros}")
     
 converter_metros()
