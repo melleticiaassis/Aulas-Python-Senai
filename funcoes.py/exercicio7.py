306425f1116540837583b4dc3267bffda32994ac
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-28 12:19:12 -0300

    Update exercicio7.py

--- funcoes.py/exercicio7.py
+++ funcoes.py/exercicio7.py
@@ -1,9 +1,11 @@
-lado_quadrado = float(input("Digite a área de um quadrado: "))
 
-area = lado_quadrado**2
-dobro = lado_quadrado*2
 
 def area_quadrado():
+    lado_quadrado = float(input("Digite a área de um quadrado: "))
+
+    area = lado_quadrado**2
+    dobro = lado_quadrado*2
+
     print(f"A area do quadrado é de {area}, e o dobro da área é de {dobro}")
     
 area_quadrado()
