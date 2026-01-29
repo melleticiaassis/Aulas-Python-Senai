7843b6d331b00c2b030c7788b010594bc3553d3a
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-29 20:26:05 -0300

    Update and rename funcao.py/funcoes Python/teste3.py to funcao.py/soma_def.py

--- funcao.py/soma_def.py
+++ funcao.py/soma_def.py
@@ -1,10 +1,9 @@
 # 1 - O que a função recebe → parâmetros
 # 2 - ela faz o calculo/decisão
 # 3 - ela devolveo return
-# 4 - e o input e print fica fora da função
-# 5 - mostre o resultado
 
 
+# Programa de soma de números usando o "def"
 def soma (a,b):
     soma = a + b
     return soma
@@ -14,4 +13,5 @@ valor2 = int(input("Digite um valor "))
 
 resultado = soma (valor1, valor2)
 print (resultado)
+
 
