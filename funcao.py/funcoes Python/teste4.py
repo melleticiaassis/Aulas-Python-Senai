c867d33e32ba0123f20d246cdfeb6b81c0e2fcd8
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-29 11:14:16 -0300

    Fix maior_numero function to compare two numbers

--- funcao.py/funcoes Python/teste4.py
+++ funcao.py/funcoes Python/teste4.py
@@ -1,5 +1,5 @@
 def maior_numero (n1 , n2):
-    if n1 > 0:
+    if n1 > n2 :
         return n1
     else:
         return n2
@@ -7,4 +7,5 @@ def maior_numero (n1 , n2):
 numero1 = int(input("Digite um valor: "))
 numero2 = int(input("Digite um valor: "))
 resultado = maior_numero(numero1,numero2)
+
 print(F"O valor é maior valor é o {resultado}")
