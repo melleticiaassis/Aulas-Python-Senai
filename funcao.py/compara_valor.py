1749bd19addc955cb0c64818f517845654607b61
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-29 20:24:38 -0300

    Update and rename funcao.py/funcoes Python/teste4.py to funcao.py/compara_valor.py

--- funcao.py/compara_valor.py
+++ funcao.py/compara_valor.py
@@ -1,3 +1,5 @@
+# Calcula maior e menor valor de um número.
+
 def maior_numero (n1 , n2):
     if n1 > n2 :
         return n1
@@ -9,3 +11,4 @@ numero2 = int(input("Digite um valor: "))
 resultado = maior_numero(numero1,numero2)
 
 print(F"O valor é maior valor é o {resultado}")
+
