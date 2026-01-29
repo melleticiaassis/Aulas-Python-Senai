fb1651ff31b5e8c67fae70c886231ea135e529da
Mel Letícia Assis <160510221+melleticiaassis@users.noreply.github.com>
2026-01-29 20:22:29 -0300

    Enhance dobro_valor.py to print the result
    Added print statement to display the result.

--- funcao.py/dobro_valor.py
+++ funcao.py/dobro_valor.py
@@ -1,7 +1,10 @@
+# Programa que calcula o dobro de um número.
+
 def dobro (numero):
     valor = (numero * 2)
     return valor
 
 valor = int(input("Digite um vlaor: "))
 resultado = dobro (valor)
+
 print(f"O dobro do valor é de {resultado}")
